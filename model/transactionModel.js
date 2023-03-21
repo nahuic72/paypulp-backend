1699fa01ace6698ba3987bf9ae94f19e3e9ee894
DanNisenson <danpablo1@gmail.com>
2023-03-21 10:25:25 +0100

    👷Update transaction model

--- model/transactionModel.js
+++ model/transactionModel.js
@@ -3,22 +3,22 @@ const QueryModel = require('./queryModel')
 class Transaction extends QueryModel {
   constructor(
     transactionId = null,
+    transactionUuid = null,
     sellerUuid = null,
     buyerUuid = null,
     paymentMethodUuid = null,
     totalAmount = null,
-    dateTime = null,
     userCompleted = null,
     wentThrough = null,
     transactionTime = null,
     geolocation = null,
   ) {
     this.transactionId = transactionId
+    this.transactionUuid = transactionUuid
     this.sellerUuid = sellerUuid
     this.buyerUuid = buyerUuid
     this.paymentMethodUuid = paymentMethodUuid
     this.totalAmount = totalAmount
-    this.dateTime = dateTime
     this.userCompleted = userCompleted
     this.wentThrough = wentThrough
     this.transactionTime = transactionTime
