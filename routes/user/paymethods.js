2003af6febf2fa475e1b64bf3370c5c9f9bdb828
DanNisenson <danpablo1@gmail.com>
2023-03-21 17:47:31 +0100

    🌟Add post new payment method logic

--- routes/user/paymethods.js
+++ routes/user/paymethods.js
@@ -3,4 +3,6 @@ const router = express.Router()
 
 router.get('/', require('../../controllers/User/PayMethods/getUserPayMethodsContr'))
 
+router.post('/', require('../../controllers/User/PayMethods/postPayMetContr'))
+
 module.exports = router
