f82074b65debc4d7673caba1ca59fc91df9e3657
DanNisenson <danpablo1@gmail.com>
2023-03-25 17:34:42 +0100

    👷add transaction update validation

--- middlewares/validationSchemas/transactionSchema.js
+++ middlewares/validationSchemas/transactionSchema.js
@@ -2,7 +2,7 @@ const { checkSchema } = require('express-validator')
 
 const locale = ['es-ES']
 
-const transactionSchema = checkSchema({
+const postTransactionSchema = checkSchema({
   sellerUuid: {
     in: ['body'],
     exists: true,
@@ -25,4 +25,47 @@ const transactionSchema = checkSchema({
   },
 })
 
-module.exports = { transactionSchema }
+const updateTransactionSchema = checkSchema({
+  paymentMethodUuid: {
+    in: ['body'],
+    optional: true,
+    notEmpty: { errorMessage: 'Payment method uuid must not be empty' },
+    isString: { errorMessage: 'Payment method uuid must be a string' },
+    isAlphanumeric: {
+      options: [locale, { ignore: '_-' }],
+      errorMessage: 'Payment method uuid uuid must only contain letters or numbers',
+    },
+    trim: true,
+  },
+  transactionUuid: {
+    in: ['body'],
+    optional: true,
+    notEmpty: { errorMessage: 'Transaction uuid must not be empty' },
+    isString: { errorMessage: 'Transaction uuid must be a string' },
+    isAlphanumeric: {
+      options: [locale, { ignore: '-' }],
+      errorMessage: 'Transaction uuid must only contain letters or numbers',
+    },
+    trim: true,
+  },
+  userCompleted: {
+    in: ['body'],
+    optional: true,
+    notEmpty: { errorMessage: 'User completed must not be empty' },
+    isBoolean: {
+      errorMessage: 'User completed must be a boolean',
+    },
+    trim: true,
+  },
+  wentThrough: {
+    in: ['body'],
+    optional: true,
+    notEmpty: { errorMessage: 'Went through must not be empty' },
+    isBoolean: {
+      errorMessage: 'Went through must be a boolean',
+    },
+    trim: true,
+  },
+})
+
+module.exports = { postTransactionSchema, updateTransactionSchema }
--- model/payMethodsModel.js
+++ model/payMethodsModel.js
@@ -7,16 +7,12 @@ class PayMethod extends QueryModel {
     userUuid = null,
     cardNumber = null,
     cardOwnerName = null,
-    cardExpiryDate = null,
-    cardCvv = null,
   ) {
     this.payMethodId = payMethodId;
     this.paymentMethodUuid = paymentMethodUuid;
     this.userUuid = userUuid;
     this.cardNumber = cardNumber;
     this.cardOwnerName = cardOwnerName;
-    this.cardExpiryDate = cardExpiryDate;
-    this.cardCvv = cardCvv;
   }
 }
 
